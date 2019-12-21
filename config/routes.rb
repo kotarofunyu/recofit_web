bc028778f04e2380905fd290a490544de5b76e28
kotarofunyu <kotarofunyu@gmail.com>
2019-12-21 15:58:02 +0900

    ・ユーザーの表示 ・ユーザー登録機能 の実装完了分。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/index'
   get '/' => "home#top"
+  get '/signup' => "users#new"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
