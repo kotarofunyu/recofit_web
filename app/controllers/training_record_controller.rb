1b0f6fa630356d722cc4c866a1749a66b307b910
kotarofunyu <kotarofunyu@gmail.com>
2019-12-24 22:07:59 +0900

    ・登録フォームと記録一覧を同じページ（index.html.erb)に統一。 ・show.html.erbをindex.html.erbに変更。それに伴うルーティングの編集/削除など。

--- app/controllers/training_record_controller.rb
+++ app/controllers/training_record_controller.rb
@@ -1,21 +1,23 @@
 class TrainingRecordController < ApplicationController
-  def new
+
+  # 作成と表示を同画面で行う
+  def index
+    # 新規作成
     @record = TrainingRecord.new
     @record.build_event
+
+    # 全ての投稿を取得して、作成日昇順でソートする
+    # @records = TrainingRecord.all.order(created_at: "DESC")
+    @records = TrainingRecord.includes(:event).order(created_at: "DESC")
+
   end
 
+  # フォームからの記録をDBに登録する処理
   def create
     @record = TrainingRecord.new(training_record_params)
     if @record.save
       redirect_to("/records")
     end
-  end
-
-  def show
-    # 全ての投稿を取得して、作成日昇順でソートする
-    # @records = TrainingRecord.all.order(created_at: "DESC")
-    @records = TrainingRecord.includes(:event)
-
   end
 
   def edit
@@ -24,8 +26,10 @@ class TrainingRecordController < ApplicationController
   def destroy
   end
 
+  # ストロングパラメーターを定義
   private
     def training_record_params
+      # 小テーブルの種目テーブルも同時にパラメータ取得
         params.require(:training_record).permit(:comment, :picture, event_attributes:[:id, :name, :weight, :rep, :set, :new])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'post' => 'training_record#new'
-  get 'records' => 'training_record#show'
+  get '/' => "home#top"
+  # 記録ここから
+  # get 'post' => 'training_record#new'
+  get 'records' => 'training_record#index'
   post 'training_record/create' => 'training_record#create'
   get 'training_record/edit'
   get 'training_record/destroy'
   get 'sessions/new'
   get 'posts/index'
-  get '/' => "home#top"
+  # ユーザーここから
   get '/signup' => "users#new"
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
