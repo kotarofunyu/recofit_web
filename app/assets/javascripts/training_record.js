246dd1411459bd7ef388bc8b1a0d3eb5596aaf54
kotarofunyu <kotarofunyu@gmail.com>
2020-01-13 01:08:48 +0900

    ・UI改善 (アイコンを積極的に使う。） ・Cocoonにおける削除機能をセットと、種目に追加。追加だけでなく「削除」もできるようにしました。

--- app/assets/javascripts/training_record.js
+++ app/assets/javascripts/training_record.js
@@ -5,8 +5,31 @@
 //   });
 // });
 $(function () {
+
+  // 種目のトグル
   $(document).on('click','.event_item_title',function() {
-    console.log('クリックしたよ');
+    // console.log('クリックしたよ');
     $(this).next('dd').slideToggle();
   });
+
+  // 種目登録のモーダル
+  $(document).on('click', 'i.fa-info-circle', function() {
+    // console.log('クリックしたよ');
+    var target = $('.modal.register_nonexist_event');
+    target.fadeIn();
+  })
+
+  $('.modalBg').click(function() {
+    console.log('クリックしたよ');
+    var target = $('.modal.register_nonexist_event');
+    target.fadeOut();
+  })
+
+  // $(document).on('click', 'modalBg', function() {
+  //   console.log('クリックしたよ');
+  //   var target = $('.modal.register_nonexist_event');
+  //   target.fadeIn();
+  // })
+
+
 });
