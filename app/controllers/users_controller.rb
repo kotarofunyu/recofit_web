f9c1a81a77cfceff6d2afe774dd7cbccd1c928da
kotarofunyu <kotarofunyu@gmail.com>
2020-01-13 16:24:47 +0900

    ・ユーザー登録機能の実装。 細かなバリデーションなどはまだ。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,15 @@ class UsersController < ApplicationController
     end
 
     def create
-        newUser = User.new(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-        if newUser.save
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "ユーザー登録が完了しました。"
             redirect_to('/records')
+        else
+            render 'new'
         end
     end
+
     private
         def user_params
             params.require(:user).permit(:name,:email,:password,:password_confirmation)
