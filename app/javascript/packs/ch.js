daccb3cb9f19bc5917bdc3d1aa26fe918b20ac63
kotarofunyu <kotarofunyu@gmail.com>
2020-02-09 19:40:37 +0900

    (1) VueChartjsへAPIから取得したデータを渡す方法を探っていたが、結局できなかった。 (2) リロードするたびにコンパイルしていたので、次回からbin/webpack-dev-serverを実行している状態で開発を行う。
    Vue.jsの基礎的な部分が圧倒的に欠けていると感じた....。
    課題を分解して、一つ一つクリアしていきます。

--- app/javascript/packs/ch.js
+++ app/javascript/packs/ch.js
@@ -10,11 +10,23 @@ document.addEventListener("DOMContentLoaded", function() {
         data: {
             records: []
         },
-        mounted :function() {
+        mounted: function() {
             axios.get('http://localhost:3000/api/training_record')
             .then(
                 response => this.records = response.data.training_record.record
-                )
+                // event = response.data.training_record.record.map(record=>record.event)
+                // console.log(event)
+                // event.forEach(function(value) {
+                //     console.log(value)
+                //     value.forEach(function(item) {
+                //         console.log(item)
+                //         item.set.forEach(function(set) {
+                //             // this.records = set.weight
+                //             console.log("重量データ"+set.weight)
+                //         })
+                //     })
+                // })
+            )
             .catch(response => console.log(response))
         }
     });
