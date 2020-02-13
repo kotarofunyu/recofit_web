7531be57869d2bc8c0cf2e70694e3c92025bfd2a
kotarofunyu <kotarofunyu@gmail.com>
2020-02-13 22:28:37 +0900

    初回ロード時にVueインスタンスが生成されない問題がありましたが、犯人が判明。 addEventListerが犯人でしたとさ。 取り除いて、無事に問題解決しました。
    これにて、グラフ実装完了！
    close #3

--- app/javascript/packs/ch.js
+++ app/javascript/packs/ch.js
@@ -1,39 +1,45 @@
 import Vue from 'vue/dist/vue.esm.js'
-import axios from 'axios'
 import { Bar } from 'vue-chartjs'
-document.addEventListener("DOMContentLoaded", function() {
+import axios from 'axios'
+
+// document.addEventListener("DOMContentLoaded", function() {
+    const url = location.href
+    const items = url.split("/")
+    const axiosGetUrl = "/api/event/"+items[4]+"?name="+items[6]
     var app = new Vue({
         el: '#chart',
         data: {
             labels: [],
-            data: []
+            data: [],
+            loading: true
         },
         methods: {
             displayGraph: function(){
                 var ctx = document.getElementById('myChart').getContext('2d');
                 var myChart = new Chart(ctx, {
-                type: 'line',
+                type: 'bar',
                 data: {
                     labels: this.labels,
                     datasets: [{
                         label: '重量の遷移',
-                        data: this.data
+                        data: this.data,
+                        backgroundColor:'#EE9428'
                     }]
                   }
                 });
-            }
+            },
         },
         mounted: function(){
-            var url = location.href
-            var items = url.split("/")
-            console.log(items[4] + items[6])
-            console.log("/api/event/"+items[4]+"?name="+items[6])
-            axios.get("/api/event/"+items[4]+"?name="+items[6]).then(response=>{
-                console.log(response)
-                this.data = response.data.weight.map(weight=>weight.weight)
-                this.labels = response.data.weight.map(weight=>weight.created_at)
-                this.displayGraph();
-            })
+            axios
+                .get(axiosGetUrl)
+                .then(response=>{
+                    console.log(response);
+                    this.data = response.data.weight.map(weight=>weight.weight);
+                    this.labels = response.data.weight.map(weight=>weight.created_at);
+                    this.loading = false;
+                    this.displayGraph();
+                    }
+                )
         }
       });
-});
+// });
