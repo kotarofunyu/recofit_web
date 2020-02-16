ef2b23df3fb3e8f5d28a6a7721f3ecb58ea4735b
kotarofunyu <kotarofunyu@gmail.com>
2020-02-17 00:32:33 +0900

    ### Issue番号 (あれば） #36 ###何をしたのか（マクロ） liの高さを揃えた。 ###具体的に何をしたのか（ミクロ） 具体的な情報部分を高さ指定して、スクロールで対応することにしました。 長期的に考えると、read moreは後々の方が良いなと思ったので、一旦これで対応します。 ###結果どうなったのか 見やすくなった。アイコンが常時表示。

--- app/models/training_record.rb
+++ app/models/training_record.rb
@@ -1,5 +1,5 @@
 class TrainingRecord < ApplicationRecord
-    validates :comment, presence: true
+    validates :comment, presence: true, length: {maximum: 100}
     mount_uploader :picture, PictureUploader
     has_many :event, dependent: :destroy
     accepts_nested_attributes_for :event,allow_destroy: true
