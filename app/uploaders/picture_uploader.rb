69c0dd318a5fa73a694cee680fc189f7fbed4e03
kotarofunyu <kotarofunyu@gmail.com>
2020-02-17 16:01:23 +0900

    ### Issue番号 (あれば） #45 ###何をしたのか（マクロ） アップロード上限サイズを設定 ###具体的に何をしたのか（ミクロ) ・上限サイズは8MB（記録アプリにしてはちょっと大きいかな..?) ・エラー文の日本語化 ###結果どうなったのか ・8MB以上はアップロードできなくなりました！
    close #45

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -35,9 +35,13 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg png)
+  end
+
+  def size_range
+    1..8.megabytes
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
