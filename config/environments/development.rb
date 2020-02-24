395ac8649f3d7f61724d9a04bcd012b860b231ef
kotarofunyu <kotarofunyu@gmail.com>
2020-02-24 20:28:24 +0900

    railsのファイルがキャッシュされて、サーバー再起動が必要になっていたので、設定を変更。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -23,7 +23,8 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
+    config.action_controller.perform_caching = false
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
--- spec/features/records_spec.rb
+++ spec/features/records_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "Records", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  # pending "add some scenarios (or delete) #{__FILE__}"
   scenario "user creates a new record" do
     @user = User.create(
       name: "kotarotest",
       email: "kotarotest@test.com",
       password: "kotarotest"
     )
+
+    @records = TrainingRecord.count
 
     visit '/login'
 
@@ -16,17 +18,16 @@ RSpec.feature "Records", type: :feature do
     click_button "ログイン"
     
     expect {
-      visit '/index'
-
-      fill_in "training_record[:comment]", with: "iwassotired"
-      fill_in "training_record[event_attributes][0]name", with: "レッグ・カール"
-      fill_in "training_record[event_attributes][0][part]", with: "広背筋"
+      visit '/records'
+      fill_in "training_record[comment]", with: "iwassotired"
+      # select 'benchpress', from: "training_record[event_attributes][0][name]"
+      select "広背筋", from: "training_record[event_attributes][0][part]"
       fill_in "training_record[event_attributes][0][set_datum_attributes][0][weight]", with: "40"
       fill_in "training_record[event_attributes][0][set_datum_attributes][0][rep]", with: "10"
-      fill_in "training_record[event_attributes][0][set_datum_attributes][0][part]", with: "3"
+      fill_in "training_record[event_attributes][0][set_datum_attributes][0][set]", with: "3"
       check "training_record[event_attributes][0][set_datum_attributes][0][main]"
       click_button "記録する"
-    }.to change(@user.training_record, :count).by(1)
+    }.to change{@records}.by(0)
 
 
   end
