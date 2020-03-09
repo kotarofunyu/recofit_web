8d55b9a836219f948da561a5747b7f4daca3c464
kotarofunyu <kotarofunyu@gmail.com>
2020-03-09 21:22:14 +0900

    Dockerfileとシェルを修正

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 # 本番環境(Amazon ECS)のタスクスタート時に以下を実行
 
 # Production環境でアセットをコンパイル
-rails assets:precompie RAILS_ENV=production
+rails assets:precompile RAILS_ENV=production
 
 # Production環境へのDBマイグレーション
 rails db:migrate RAILS_ENV=production
