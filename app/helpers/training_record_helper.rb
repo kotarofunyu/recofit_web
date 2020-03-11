650b06ec5bfcd380992bc15bf7165f75c290b2ee
kotarofunyu <kotarofunyu@gmail.com>
2020-03-11 21:25:54 +0900

    重量のデータ型をinteger→floatへ変更。

--- app/helpers/training_record_helper.rb
+++ app/helpers/training_record_helper.rb
@@ -1,2 +1,7 @@
 module TrainingRecordHelper
+    # 重量数値の小数点以下が０の場合に整数へ変換する
+    def weightInt(weight)
+        decimal = weight - weight.to_i
+        decimal == 0 ? weight.to_i : weight
+    end
 end
