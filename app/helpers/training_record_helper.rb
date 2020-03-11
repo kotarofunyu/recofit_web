180dc7509f3391984a93e9d9759c8e26cc73af05
kotarofunyu <kotarofunyu@gmail.com>
2020-03-11 21:30:53 +0900

    rubocopエラー修正

--- app/helpers/training_record_helper.rb
+++ app/helpers/training_record_helper.rb
@@ -1,7 +1,7 @@
 module TrainingRecordHelper
     # 重量数値の小数点以下が０の場合に整数へ変換する
-    def weightInt(weight)
+    def weight_change(weight)
         decimal = weight - weight.to_i
-        decimal == 0 ? weight.to_i : weight
+        decimal.zero? ? weight.to_i : weight
     end
 end
