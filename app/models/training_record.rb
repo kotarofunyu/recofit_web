dae46da9eb935058cda0fb464a521139e623e0a8
kotarofunyu <kotarofunyu@gmail.com>
2020-03-24 03:09:00 +0900

    rubocopのエラーを修正

--- app/models/training_record.rb
+++ app/models/training_record.rb
@@ -2,10 +2,6 @@ class TrainingRecord < ApplicationRecord
     has_many :event, dependent: :destroy
     accepts_nested_attributes_for :event, allow_destroy: true
     validates :comment, presence: true, length: { maximum: 100 }
-    validates :event, presence: {
-        message: ->  (rec, data) {
-            I18n.t('activemodel.errors.models.training_record.childrenover')
-        }
-    }
+    validates :event, presence: true
     mount_uploader :picture, PictureUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name, presence: true
-    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL, message: "は正しい形式で入力してください" }
+    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL, message: ' は正しい形式で入力してください' }
     has_secure_password
     mount_uploader :picture, PictureUploader
     def full_name
