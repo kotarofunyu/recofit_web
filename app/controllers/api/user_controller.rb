4a777a848808cc05f1229b8cd06ebddf23a02270
kotarofunyu <kotarofunyu@gmail.com>
2020-05-06 02:36:06 +0900

    API UserへのGETのパラメーターをapi_tokenへ変更

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -2,9 +2,13 @@ class Api::UserController < ApplicationController
   # skip_before_action :verify_authenticity_token
   skip_forgery_protection
 
-  def index
-    @user = User.all
+  def show
+    @user = User.find_by(api_token: params[:api_token])
   end
+
+  # def index
+  #   @user = User.all
+  # end
 
   def create
     user = User.new(user_params)
@@ -14,10 +18,6 @@ class Api::UserController < ApplicationController
     else
       render json: { status: 'ERROR', data: user.errors }
     end
-  end
-
-  def show
-    @user = User.find_by(id: params[:id])
   end
 
   private
