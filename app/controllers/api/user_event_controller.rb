6be545192eb838817e14f82d48e817e8641402ab
kotarofunyu <kotarofunyu@gmail.com>
2020-05-10 08:25:27 +0900

    rubocopのエラーを修正

--- app/controllers/api/user_event_controller.rb
+++ app/controllers/api/user_event_controller.rb
@@ -2,7 +2,10 @@ class Api::UserEventController < ApplicationController
   def show
     user = User.find_by(api_token: params[:api_token])
     # events = Event.all
-    events = Event.joins(:training_record).where(training_records: { user_id: user.id }).select('events.name').distinct
+    events =
+      Event.joins(:training_record).where(
+        training_records: { user_id: user.id }
+      ).select('events.name').distinct
     if events
       render json: events
     else
--- app/controllers/api/user_record_controller.rb
+++ app/controllers/api/user_record_controller.rb
@@ -1,5 +1,4 @@
 class Api::UserRecordController < ApplicationController
-
   def show
     user = User.find_by(api_token: params[:api_token])
     @records = TrainingRecord.where(user_id: user.id)
