766e3a2000c1d93074d4ff285ec384004109da8f
kotarofunyu <kotarofunyu@gmail.com>
2020-07-05 21:36:03 +0900

    pumaとnginxのソケット通信をオフ（herokuにデプロイするため）

--- config/puma.rb
+++ config/puma.rb
@@ -33,6 +33,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-app_root = File.expand_path("../..", __FILE__)
-bind "unix:///#{app_root}/tmp/sockets/puma.sock"
-stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+# app_root = File.expand_path("../..", __FILE__)
+# bind "unix:///#{app_root}/tmp/sockets/puma.sock"
+# stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
