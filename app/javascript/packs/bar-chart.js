cf6657a4c6b9ccab1e67f6724536c882d7fd3edc
kotarofunyu <kotarofunyu@gmail.com>
2021-10-12 22:07:24 +0900

    webpack周りをいろいろいじってる...

--- app/javascript/packs/bar-chart.js
+++
@@ -1,8 +0,0 @@
-import { Bar } from 'vue-chartjs'
-export default {
-  extends: Bar,
-  props: ['chartData', 'options'], // propsに chartData と options を定義
-  mounted () {
-    this.renderChart(this.chartData, this.options)
-  }
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,57 @@
+// const path = require("path");
+// const WebpackAssetsManifest = require("webpack-assets-manifest");
+
+// const { NODE_ENV } = process.env;
+// const isProd = NODE_ENV === "production";
+
+// module.exports = {
+//   mode: isProd ? "production" : "development",
+//   devtool: "source-map",
+//   entry: {
+//     application: path.resolve(__dirname, "app/javascript/application.js"),
+//   },
+//   output: {
+//     path: path.resolve(__dirname, "public/packs"),
+//     publicPath: "/packs/",
+//     filename: isProd ? "[name]-[hash].js" : "[name].js",
+//   },
+//   resolve: {
+//     extensions: [".js"],
+//   },
+//   plugins: [
+//     new WebpackAssetsManifest({
+//       publicPath: true,
+//       output: "manifest.json",
+//     }),
+//   ],
+// };
 const path = require("path");
 const WebpackAssetsManifest = require("webpack-assets-manifest");
+const glob = require("glob");
 
 const { NODE_ENV } = process.env;
 const isProd = NODE_ENV === "production";
 
+const entries = {};
+glob.sync("app/javascript/packs/*.js").forEach((filePath) => {
+  const name = path.basename(filePath, path.extname(filePath));
+  entries[name] = path.resolve(__dirname, filePath);
+});
+
 module.exports = {
   mode: isProd ? "production" : "development",
   devtool: "source-map",
-  entry: {
-    application: path.resolve(__dirname, "app/javascript/application.js"),
-  },
+  entry: entries,
   output: {
     path: path.resolve(__dirname, "public/packs"),
     publicPath: "/packs/",
     filename: isProd ? "[name]-[hash].js" : "[name].js",
+  },
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks: "initial",
+    },
   },
   resolve: {
     extensions: [".js"],
@@ -22,6 +60,7 @@ module.exports = {
     new WebpackAssetsManifest({
       publicPath: true,
       output: "manifest.json",
+      writeToDisk: true
     }),
   ],
 };
