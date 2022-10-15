b60db21f6514c9cb61419c857d0b4e6a82fd85a1
kotarofunyu <kotarofunyu@gmail.com>
2022-10-15 21:58:29 +0900

    bundle exec rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,26 +1,22 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-# config.assets.initialize_on_precompile = false
 
 module Kochanapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # 時間帯を日本に変更
-    config.time_zone = 'Tokyo'
-
-    # 日本語
-    config.i18n.default_locale = :ja
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
