32eb87df1fb9d334db2e836cf204db69009e182f
kotarofunyu <kotarofunyu@gmail.com>
2022-10-19 21:26:54 +0900

    TrainingRecordモデルとUserモデルとの関連付けを設定
    UserからTrainingRecordモデルへの関連付けは定義されていたが、
    その逆が定義されていなかったため追加した

--- app/models/training_record.rb
+++ app/models/training_record.rb
@@ -1,5 +1,6 @@
 class TrainingRecord < ApplicationRecord
     has_many :event, dependent: :destroy
+    belongs_to :user
     accepts_nested_attributes_for :event, allow_destroy: true
     validates :comment, presence: true, length: { maximum: 100 }
     validates :event, presence: true
